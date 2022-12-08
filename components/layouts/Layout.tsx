a5a640107c07250f4477e8375d2a007809281343
Jimmy Reyes <jimmyreyes1983@hotmail.com>
2022-12-08 02:17:21 +0100

    NextUI Implementado

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+
+export const Layout = () => {
+  return (
+    <>
+        <Head>
+            <title>Pokemon App</title>
+        </Head>
+    
+    
+    
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@nextui-org/react";
 
 
 export default function HomePage() {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      {/* <h1>Hola Mundo</h1> */}
+      <Button color="gradient">
+        Hola mundo
+      </Button>
     </>
   )
 }
